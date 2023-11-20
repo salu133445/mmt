62168be8e861b6f174f19daa4919cd4a779b6acd
InabaTatsuro <tatsuro.pianooo@gmail.com>
2023-11-20 18:46:57 +0900

    Fix missing dot in with_suffix method call

--- mmt/extract.py
+++ mmt/extract.py
@@ -72,10 +72,10 @@ def extract(name, in_dir, out_dir, resolution):
     (out_dir / name).parent.mkdir(exist_ok=True)
 
     # Save the notes as a CSV file
-    representation.save_csv_notes((out_dir / name).with_suffix("csv"), notes)
+    representation.save_csv_notes((out_dir / name).with_suffix(".csv"), notes)
 
     # Save the notes as a NPY file
-    np.save((out_dir / name).with_suffix("npy"), notes)
+    np.save((out_dir / name).with_suffix(".npy"), notes)
 
     return name
 
